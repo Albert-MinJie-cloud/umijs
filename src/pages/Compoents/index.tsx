13f88536e11c4d97b6281e45a6ebc4196891ecd7
闵杰 <minjie@192.168.0.125>
2023-05-31 23:50:55 +0800

    feat:组件初始化

---
+++ src/pages/Compoents/index.tsx
@@ -0,0 +1,13 @@
+import { PageContainer } from '@ant-design/pro-components';
+import { Select } from 'antd';
+
+const Compoents: React.FC = () => {
+  return (
+    <PageContainer ghost>
+      <div>1</div>
+      <Select></Select>
+    </PageContainer>
+  );
+};
+
+export default Compoents;
